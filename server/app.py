342fabd62ca64bbb0d57363c938616a495412582
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-01-30 14:52:51 -0500

    done

--- server/app.py
+++ server/app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,13 +14,39 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = Message.query.order_by(Message.created_at)
+        messages_serialized = [message.to_dict() for message in messages]
+        return make_response(messages_serialized, 200)
+    elif request.method == "POST":
+        data = request.get_json()
+        new_message = Message(body=data["body"], username=data["username"])
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(), 201)
 
-@app.route('/messages/<int:id>')
+
+@app.route("/messages/<int:id>", methods=["GET", "PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
 
-if __name__ == '__main__':
-    app.run(port=5555)
+    if request.method == "GET":
+        return make_response(message.to_dict(), 200)
+    elif request.method == "PATCH":
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+        db.session.add(message)
+        db.session.commit()
+        return make_response(message.to_dict(), 200)
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        return make_response({"message": "message successfully deleted"}, 200)
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
